c5cd1ce826fa879040dfaaed905e41ce8491d486
Mallory Mousson <mmousson@protonmail.com>
2024-08-03 17:22:22 +0200

    Program can turn the OLED screen on ! :)

--- SSD1306.c
+++ SSD1306.c
@@ -9,68 +9,102 @@
 #include "LCDDebug.h"
 
 #define I2C_BaudRate          100000
+#define START_PAGE_ADDR           0
+#define END_PAGE_ADDR             7     // 7 for 128x64, 3 for 128x32 version
+#define START_COLUMN_ADDR         0
+#define END_COLUMN_ADDR           127
+#define RAM_X_END                 END_COLUMN_ADDR + 1
+#define RAM_Y_END                 END_PAGE_ADDR + 1
+#define CACHE_SIZE_MEM            (1 + END_PAGE_ADDR) * (1 + END_COLUMN_ADDR)
 
-static void I2C_MasterWait()
+uint8_t     gotNack  = 0;
+
+static void I2C_WaitForBus()
 {
     while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F));
 }
 
-static void I2C_MasterStart()
+static void I2C_TransactionStart()
 {
-    I2C_MasterWait();
+    I2C_WaitForBus();
     SEN = 1;
 }
 
-static void I2C_MasterStop()
+static void I2C_TransactionStop()
 {
-    I2C_MasterWait();
+    I2C_WaitForBus();
     PEN = 1;
 }
 
-static unsigned char I2C_MasterWrite(unsigned char byte)
+static void I2C_ByteWrite(unsigned char byte)
 {
-    I2C_MasterWait();
+    if (gotNack)
+        return;
+
+    I2C_WaitForBus();
     SSPIF = 0;
     SSPBUF = byte;
     while (!SSPIF);
     SSPIF = 0;
-    return (ACKSTAT);
+    
+    if (ACKSTAT)
+    {
+        gotNack = 1;
+        I2C_TransactionStop();
+        lcdClearline(1);
+        lcdString("Got NACK ?!?");
+        __delay_ms(1000);
+    }
+}
+
+static void I2C_SendCommand(uint8_t cmd)
+{
+    uint8_t ack1, ack2, ack3;
+
+    I2C_TransactionStart();
+    I2C_ByteWrite((SSD1306_ADDR << 1) | SSD1306_REQ_WRITE);
+    I2C_ByteWrite(SSD1306_CONTROL_CMD_STREAM);
+    I2C_ByteWrite(cmd);
+    I2C_TransactionStop();
 }
 
 void SSD1306_Init()
 {
-    unsigned char result;
+    int i = 0;
+    unsigned char ack;
 
     lcdSetCursor(0, 1);
-    
     SSPCON1 = 0x28;
     SSPCON2 = 0x00;
     SSPSTAT = 0x00;
     SSPADD = ((_XTAL_FREQ/4)/I2C_BaudRate) - 1;
-    //SCL_bit = 1;
-    //SDA_bit = 1;
-
-    //I2C_MasterStart();
-    //result = I2C_MasterWrite(SSD1306_ADDR);
-    //I2C_MasterStop();
     
-    while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F));
-    SEN = 1;
-    while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F));
-    SSPIF = 0;
-    SSPBUF = 0b01111000;
-    while (!SSPIF);
-    SSPIF = 0;
-    result = ACKSTAT;
-    while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F));
-    PEN = 1;
+    I2C_SendCommand(SSD1306_DISPLAY_OFF);
+    I2C_SendCommand(SSD1306_SET_DISPLAY_CLOCK_DIV_RATIO);
+    I2C_SendCommand(0x80);
+    I2C_SendCommand(SSD1306_SET_MULTIPLEX_RATIO);
+    I2C_SendCommand(64 - 1);
+    I2C_SendCommand(SSD1306_SET_DISPLAY_OFFSET);
+    I2C_SendCommand(0x00);
+    I2C_SendCommand(SSD1306_SET_START_LINE);
+    I2C_SendCommand(SSD1306_CHARGE_PUMP);
+    I2C_SendCommand(0x14);
+    I2C_SendCommand(SSD1306_MEMORY_ADDR_MODE);
+    I2C_SendCommand(0x00);
+    I2C_SendCommand(SSD1306_SET_SEGMENT_REMAP | 0x01);
+    I2C_SendCommand(SSD1306_COM_SCAN_DIR_DEC);
 
-    if (result) {
-        lcdClearline(1);
-        lcdString("NACK");
-    } else {
-        lcdClearline(1);
-        lcdString("ACK Received!");
-    }
-    __delay_ms(1000);
+    I2C_SendCommand(SSD1306_SET_COM_PINS);
+    I2C_SendCommand(0x02);
+    I2C_SendCommand(SSD1306_SET_CONTRAST_CONTROL);
+    I2C_SendCommand(0x8F);
+    
+    I2C_SendCommand(SSD1306_SET_PRECHARGE_PERIOD);
+    I2C_SendCommand(0xF1);
+    I2C_SendCommand(SSD1306_SET_VCOM_DESELECT);
+    I2C_SendCommand(0x40);
+    I2C_SendCommand(SSD1306_DISPLAY_ALL_ON_RESUME);
+    I2C_SendCommand(SSD1306_NORMAL_DISPLAY);
+    I2C_SendCommand(SSD1306_DEACTIVATE_SCROLL);
+    I2C_SendCommand(SSD1306_DISPLAY_ON);
 }
--- SSD1306.h
+++ SSD1306.h
@@ -12,8 +12,29 @@
 
 # include <xc.h> // include processor files - each processor file is guarded.
 
-# define SSD1306_ADDR 0x3C                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-//# define SSD1306_ADDR 0x3D    //Uncomment if SA0 is set
+# define SSD1306_ADDR                           0b0111100
+# define SSD1306_REQ_WRITE                      0x0
+# define SSD1306_REQ_READ                       0x1
+# define SSD1306_CONTROL_CMD_STREAM             0b00000000
+# define SSD1306_CONTROL_DATA_STREAM            0b01000000
+
+# define SSD1306_DISPLAY_OFF                    0xAE
+# define SSD1306_SET_DISPLAY_CLOCK_DIV_RATIO    0xD5
+# define SSD1306_SET_MULTIPLEX_RATIO            0xA8
+# define SSD1306_SET_DISPLAY_OFFSET             0xD3
+# define SSD1306_SET_START_LINE                 0x40
+# define SSD1306_CHARGE_PUMP                    0x8D
+# define SSD1306_MEMORY_ADDR_MODE               0x20
+# define SSD1306_SET_SEGMENT_REMAP              0xA0
+# define SSD1306_COM_SCAN_DIR_DEC               0xC8
+# define SSD1306_SET_COM_PINS                   0xDA
+# define SSD1306_SET_CONTRAST_CONTROL           0x81
+# define SSD1306_SET_PRECHARGE_PERIOD           0xD9
+# define SSD1306_SET_VCOM_DESELECT              0xDB
+# define SSD1306_DISPLAY_ALL_ON_RESUME          0xA4
+# define SSD1306_NORMAL_DISPLAY                 0xA6
+# define SSD1306_DEACTIVATE_SCROLL              0x2E
+# define SSD1306_DISPLAY_ON                     0xAF
 
 void SSD1306_Init();
 
